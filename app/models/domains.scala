2bf7f3285c4c5e348ae47620c5b4e6956fe54fc1
Lance Gatlin <lance.gatlin@gmail.com>
2015-05-19 22:07:29 -0400

    fixed compile errors & dbInit

--- app/models/domains.scala
+++ app/models/domains.scala
@@ -23,8 +23,6 @@ case class User(
 )
 
 object User{
-  import JsonFormats.format_BSONObjectID
-  implicitly[Format[BSONObjectID]]
   implicit val format_User = Json.format[User]
 }
 
@@ -74,36 +72,3 @@ case class Page[A](items: Seq[A], page: Int, offset: Long, total: Long) {
   lazy val next = Option(page + 1).filter(_ => (offset + items.size) < total)
 }
 
-object JsonFormats {
-  import play.api.libs.json.Json
-  import play.api.data._
-  import play.api.data.Forms._
-  import play.modules.reactivemongo.json.BSONFormats._
-
-  // Generates Writes and Reads for Feed and User thanks to Json Macros
-  implicit val employeeFormat = Json.format[Employee]
-
-  implicit val format_DateTime = new Format[DateTime]{
-    override def reads(json: JsValue): JsResult[DateTime] = {
-      json match {
-        case JsNumber(n) => JsSuccess(new DateTime(n))
-        case _ => JsError("Not a number")
-      }
-    }
-
-    override def writes(o: DateTime): JsValue = JsNumber(o.getMillis)
-  }
-
-  implicit val format_BSONObjectID = new Format[BSONObjectID]{
-    override def reads(json: JsValue): JsResult[BSONObjectID] = {
-      json match {
-        case JsString(n) => JsSuccess(BSONObjectID(n))
-        case _ => JsError("Not a string")
-      }
-    }
-
-    override def writes(o: BSONObjectID): JsValue = JsString(o.stringify)
-  }
-  //implicit val userFormat = Json.format[User]
-
-}
